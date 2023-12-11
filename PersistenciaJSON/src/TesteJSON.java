f2ad75c46eecbf8808622d4707d8d35bad3b806c
luarapat <luara.patracon.dev@gmail.com>
2023-12-11 10:20:27 -0300

    PWBE1-Projeto

---
+++ PersistenciaJSON/src/TesteJSON.java
@@ -0,0 +1,21 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+public class TesteJSON {
+	public static void main (String[] args) {
+		try {
+			ObjectMapper objeto = new ObjectMapper();
+			//objeto para utilizar na serialização
+			Pessoa pes = new Pessoa("Abel", 45);
+			//serializar obj pes
+			String json = objeto.writeValueAsString(pes);
+			System.out.println("Objeto serializado");
+			System.out.println(json);
+			//desserializar o JSON para o objeto pes
+			Pessoa desPessoa = objeto.readValue(json, Pessoa.class);
+			System.out.println("JSON desserializado para obj pessoa:");
+			System.out.println("Nome: " + desPessoa.getNome());
+			System.out.println("Idade: "+ desPessoa.getIdade());
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
